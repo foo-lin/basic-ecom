db2db55d76ab670c7d23acc711581e5a1467cce1
foo-lin <foolin995@gmail.com>
2020-02-21 21:20:33 +0530

    Add clinet with vue

---
+++ client/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import '@inkline/inkline/dist/inkline.css'
+import Inkline from '@inkline/inkline'
+
+Vue.use(Inkline);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
